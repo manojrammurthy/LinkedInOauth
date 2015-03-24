26ac2c66441e5614ae79ba10db350b47f37bbd92
manojrammurthy <manrox.drag@gmail.com>
2015-03-24 12:12:35 +0530

    few fixes in redirection done

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 	<div style="text-align:center">
-	<a href="linkedin_login.php"><img src="images/linkein_login.png" /></a>
+	<a href="linkedin_login.php"><img src="images/linkedin_login.png" /></a>
 	<!-- <br/>
 		<a href="http://www.9lessons.info">www.9lessons.info</a>
 	</div> -->
