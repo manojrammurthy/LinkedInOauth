8073582876c86a9c1e443a5e95f2decebde3c414
manojrammurthy <manrox.drag@gmail.com>
2015-03-24 11:58:15 +0530

    index page created

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Linkedin login using php </title>
+</head>
+<body>
+	<div style="text-align:center">
+	<a href="linkedin_login.php"><img src="images/linkein_login.png" /></a>
+	<!-- <br/>
+		<a href="http://www.9lessons.info">www.9lessons.info</a>
+	</div> -->
+	
+</body>
+</html>
