149efa163311239016f7fb815feb16ee277db80a
manojrammurthy <manrox.drag@gmail.com>
2015-04-03 14:37:15 +0530

    basic profile fetching done

--- Linkedin_Lib/linkedinConfig.php
+++ Linkedin_Lib/linkedinConfig.php
@@ -1,6 +1,6 @@
 <?php
 $config = array('client_id' => '77bs9e9076w1o5',							//  Linkedin client ID
-				'client_secret' => '',	//  Linkedin client SECRET
+				'client_secret' => 'IE1tURoynf1cYeWZ',	//  Linkedin client SECRET
 				'redirect_url' => 'http://localhost/LinkedInOauth/access.php',	//  Linkedin Redirect URL (As mentioned in linkedin APP PAGE)
 				'app_name' => 'insta');								//  linkedin App Name (As mentioned in linkedin APP PAGE)
 ?>
--- access.php
+++ access.php
@@ -9,14 +9,44 @@ $git = new linkedinApi($config);
 $new = $git->access();
 $accessT = $new['access_token'];
 $expiresT = $new['expires_in'];
-echo "access_token =".$accessT ."<br>";
-echo "<br> expiry time = ".$expiresT."<br>";
+$_SESSION['access_token']= $accessT;
+// echo "access_token =".$accessT ."<br>";
+// echo "<br> expiry time = ".$expiresT."<br>";
   
 }
  else 
   {
   	echo "CSRF attack ";
+  	exit;
   }
+$user = fetch('GET', '/v1/people/~:(firstName,lastName)');
+print "Hello $user->firstName $user->lastName.";
+exit;
 
-
+function fetch($method, $resource, $body = '') {
+    //print $_SESSION['access_token'];
+ 
+    $opts = array(
+        'http'=>array(
+            'method' => $method,
+            'header' => "Authorization: Bearer " . $_SESSION['access_token'] . "\r\n" . "x-li-format: json\r\n"
+        )
+    );
+ 
+    // Need to use HTTPS
+    $url = 'https://api.linkedin.com' . $resource;
+ 
+    // Append query parameters (if there are any)
+    if (count($params)) { $url .= '?' . http_build_query($params); }
+ 
+    // Tell streams to make a (GET, POST, PUT, or DELETE) request
+    // And use OAuth 2 access token as Authorization
+    $context = stream_context_create($opts);
+ 
+    // Hocus Pocus
+    $response = file_get_contents($url, false, $context);
+ 
+    // Native PHP object, please
+    return json_decode($response);
+}
 ?>
